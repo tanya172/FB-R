1dbc055c8d5db0dd5a5888342e0565cd44fc2bbc
tanya172 <tannyaverma086@gmail.com>
2023-07-17 21:50:36 +0530

    signin page complete

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "../firebase";
+
+const auth = getAuth(app);
 
 const SigninPage = () => {
-    return(
-        <div className="signin-page">
-        <label>Enter you email</label>
-        <input type="email" placeholder="Enter your email"/>
-        <label>Enter your password</label>
-        <input type="password" placeholder="Enter your password"/>
-        <button>Signin</button>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        </div>
-    );
-}
+  const signinUser = () => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((value) => console.log("Signin successful"))
+    .catch((err)=> console.log(err));
+    
+  };
+
+  return (
+    <div className="signin-page">
+      <label>Enter you email</label>
+      <input
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email"
+        placeholder="Enter your email"
+      />
+
+      <label>Enter your password</label>
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password"
+        placeholder="Enter your password"
+      />
+      <button onClick={signinUser}>Signin</button>
+    </div>
+  );
+};
 
 export default SigninPage;
